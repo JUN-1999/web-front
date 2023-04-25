32a03bb7cead8dfc7f8d123d6102f0c4094ddb34
JUN-1999 <1173943332@qq.com>
2023-04-25 21:45:05 +0800

    将git仓库转到github，并且去除练习数据 =wjc

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,19 +1,14 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import DemoView from '../views/Demo/DemoView.vue'
+
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: import.meta.env.MODE === 'learn' ? DemoView : HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/AboutView.vue')
+      component: HomeView
     },
     {
       path: '/ElectronicWoodfish',
@@ -25,7 +20,7 @@ const router = createRouter({
       name: 'Jw3',
       component: () => import('../views/Jw3/Index.vue')
     },
-    { path: '*', redirect: '/' }
+    { path: '/:catchAll(.*)', redirect: '/' }
   ]
 })
 
